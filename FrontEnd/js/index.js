a4c385062453439e22d489d462127362b71daa4c
VincentC27 <cardon.vincent@hotmail.fr>
2024-03-11 13:00:23 +0100

    display Categories OK

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,41 +1,44 @@
 const gallery = document.querySelector(".gallery")
+const filter = document.querySelector(".filters")
 
 //  -- TODO: rassembler les 2 func en 1
-// Récuperer les travaux 
-async function getWorks() {
+// Récuperer les travaux et les afficher
+async function displayWorks() {
     try {
         const responseWorks = await fetch("http://localhost:5678/api/works")
         .then(responseWorks => responseWorks.json());
-        return await responseWorks;
+        
+        responseWorks.forEach(work => {
+            const figure = document.createElement("figure")
+            const img = document.createElement("img")
+            const figcaption = document.createElement("figcaption")
+            img.src = work.imageUrl;
+            figcaption.textContent = work.title;
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            gallery.appendChild(figure);
+        })
         
     } catch (error) {
-        console.error("Une erreur est survenue pendant la récupération des travaux", error);
-        
+        console.error("Une erreur est survenue pendant la récupération des travaux", error);    
     }
 }
-// Afficher les travaux
-async function displayWorks() {
-    const arrayWorks = await getWorks()
-
-    arrayWorks.forEach(work => {
-        const figure = document.createElement("figure")
-        const img = document.createElement("img")
-        const figcaption = document.createElement("figcaption")
-        img.src = work.imageUrl;
-        figcaption.textContent = work.title;
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
-    });
-}
 displayWorks();
+
 
 // Récupérer et afficher les categories
 async function displayCategories() {
     try {
         const responseCategories = await fetch("http://localhost:5678/api/categories")
         .then(responseCategories => responseCategories.json());
-        console.log(responseCategories);
+        
+        responseCategories.forEach(category => {
+            const btn = document.createElement("button");
+            btn.textContent = category.name;
+            btn.id = category.id;
+            btn.classList.add("filter-btn");
+            filter.appendChild(btn);
+        })
         
     } catch (error) {
         console.error("Une erreur est survenue pendant la récupération des catégories", error);
