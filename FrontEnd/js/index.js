8350eb812c05a3573d5847a696f57e866894ece3
VincentC27 <cardon.vincent@hotmail.fr>
2024-03-11 12:04:49 +0100

    display works OK

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,14 +1,33 @@
-// Récuperer travaux 
+const gallery = document.querySelector(".gallery")
+
+// Récuperer les travaux 
 async function getWorks() {
     try {
         const responseWorks = await fetch("http://localhost:5678/api/works")
         .then(responseWorks => responseWorks.json());
-        console.log(responseWorks);
+        return await responseWorks;
         
     } catch (error) {
         console.error("Une erreur est survenue", error);
         
     }
-
 }
-getWorks();
+
+// Afficher les travaux
+async function displayWorks() {
+    const arrayWorks = await getWorks()
+    // console.log(arrayWorks);
+
+    arrayWorks.forEach(work => {
+        const figure = document.createElement("figure")
+        const img = document.createElement("img")
+        const figcaption = document.createElement("figcaption")
+        img.src = work.imageUrl;
+        figcaption.textContent = work.title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
+}
+
+displayWorks();
