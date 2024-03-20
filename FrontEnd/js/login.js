ceae6cb94aa7c2137cf435582d7aaf4a60787791
VincentC27 <cardon.vincent@hotmail.fr>
2024-03-20 12:29:09 +0100

    test login error form

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,10 +1,7 @@
 const form = document.querySelector("form");
-const email = document.querySelector("form #email");
-const password = document.querySelector("form #pass");
-
-// affiche les msg d'erreur
-const errorMsg = document.querySelector("#contact p");
-// console.log(errorMsg);
+let errorMsg = document.createElement("span");
+let inputEmail = document.querySelector("[name='email']");
+let inputPassword = document.querySelector("[name='password']");
 
 form.addEventListener("submit", async function (event) {
     // évite le rechargement de la page quand on envoie le form
@@ -13,6 +10,20 @@ form.addEventListener("submit", async function (event) {
 
 // On récupère les valeurs dans login
 const login = {
-    email: document.querySelector("[name='email']").value,
-    password: document.querySelector("[name='password']").value,
+    email: inputEmail.value,
+    password: inputPassword.value,
+};
+console.log(login);
+// vérifie que les champs ne sont pas vide
+if (!login.email || !login.password) {
+    inputPassword.insertAdjacentElement("afterend",spanElement);
+    spanElement.className = "message-error-login";
+    spanElement.innerText = "Veuillez compléter l'ensemble des champs à saisir";
+} else {
+    // envoi information sur l'API login
+    const responseLogin = await fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(login),
+    });
 };
