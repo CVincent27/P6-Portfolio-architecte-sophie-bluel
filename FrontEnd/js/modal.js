3e8fca8d3e692ebe07b3313fcdca18ea2355afd6
VincentC27 <cardon.vincent@hotmail.fr>
2024-03-26 11:20:13 +0100

    test 1ere modale

---
+++ FrontEnd/js/modal.js
@@ -0,0 +1,7 @@
+// Première modale
+const modalProjet = document.querySelector("#modal-projet");
+const modalContainer = document.createElement("div");
+modalContainer.className = "modal-container";
+const imgModalContainer = document.createElement("i");
+imgModalContainer.id = "first-modal-close";
+imgModalContainer.className = "fa-solid fa-xmark";
