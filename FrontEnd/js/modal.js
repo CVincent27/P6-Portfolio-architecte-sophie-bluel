b5269ed177f6d242a3e511946e8363e396ef0299
VincentC27 <cardon.vincent@hotmail.fr>
2024-04-15 10:52:50 +0200

    update logique modal

--- FrontEnd/js/modal.js
+++ FrontEnd/js/modal.js
@@ -1,5 +1,10 @@
 import { getWorks, deleteWork, getCategories } from './service.js';
 
+//pour affichage message erreur
+let spanElement = document.createElement("span");
+spanElement.innerText = "";
+spanElement.className = "";
+
 // ---- Première modale ----
 
 // récupération de la modale html
@@ -38,24 +43,7 @@ export async function displayWorksModal() {
 }
 displayWorksModal()
 
-
-// ---- Seconde modale ----
-
-// récupère la seconde modale
-const modalProjetPhoto = document.querySelector("#modal-projet-photo");
-// ouvrir 2nd depuis 1ere modale
-const btnOpenSecondModal = document.querySelector(".btn-add-photo");
-
-// ouverture de la seconde modale (via 1ere modal)
-btnOpenSecondModal.addEventListener("click", function () {
-    // Ferme la première modale
-    modalProjet.close();
-    // Ouvre la seconde modale
-    modalProjetPhoto.showModal();
-});
-
-
-//fonctionnement modale
+// fonctionnement modale
 
 // Open modale
 var btnOpenModal = document.getElementById("modal-projet-btn");
@@ -94,14 +82,66 @@ btnBackModal.addEventListener("click", function () {
     modalProjet.showModal();
 });
 
+
+// ---- Seconde modale ----
+
+// récupère la seconde modale
+const modalProjetPhoto = document.querySelector("#modal-projet-photo");
+// ouvrir 2nd depuis 1ere modale
+const btnOpenSecondModal = document.querySelector(".btn-add-photo");
+
+// ouverture de la seconde modale (via 1ere modal)
+btnOpenSecondModal.addEventListener("click", function () {
+    // Ferme la première modale
+    modalProjet.close();
+    // Ouvre la seconde modale
+    modalProjetPhoto.showModal();
+});
+
+// upload img et verif taille et type de fichier
+const newImage = document.querySelector("#myfile");
+var uploadedImage = "";
+
+newImage.addEventListener("change", function () {
+    spanElement.className = "";
+    spanElement.innerText = "";
+    const reader = new FileReader();
+    const type = document.getElementById("myfile").files[0].type;
+    if (type !== "image/png" && type !== "image/jpeg" && type !== "image/jpg") {
+        // Affichage d'une erreur si le type de fichier n'est pas pris en charge
+    } else {
+        reader.addEventListener("load", () => {
+            // Cacher la div de classe "form-upload-photo"
+            const formUploadPhotoDiv = document.querySelector(".form-upload-photo");
+            formUploadPhotoDiv.style.display = "none";
+
+            // Création d'une nouvelle balise img
+            const imgElement = document.createElement("img");
+            imgElement.src = reader.result;
+            imgElement.id = "display-image";
+
+            // Récupérer la div de class "add-form-photo"
+            const formAddPhotoDiv = document.querySelector(".form-add-photo");
+
+            // Insérer la balise img dans la div de class "add-form-photo"
+            formAddPhotoDiv.appendChild(imgElement);
+        });
+        reader.readAsDataURL(this.files[0]);
+    }
+});
+
+
+
+
+
+
+
+
 // récupérer et afficher les catégories dans le select
 async function CategoriesSelect() {
     try {
         const categories = await getCategories(); //
         const selectCategorieFormAddPhoto = document.getElementById("select-categorie");
-
-        // delete les anciennes options du select
-        selectCategorieFormAddPhoto.innerHTML = "";
 
         // ajoute chaque categories dans une option
         categories.forEach(categorie => {
