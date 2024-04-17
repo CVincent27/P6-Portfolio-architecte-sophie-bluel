d7026fbdfdacf7a109eb596774944c892b4e9ae1
VincentC27 <cardon.vincent@hotmail.fr>
2024-04-17 12:40:23 +0200

    update error-msg login

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -3,27 +3,30 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", async function (event) {
     // évite le rechargement de la page quand on envoie le form
-    event.preventDefault(); 
+    event.preventDefault();
 
-    let errorMsg = document.createElement("span");
+    let errorMsg = document.querySelector("#login-error");
+
+    if (!errorMsg) {
+        errorMsg = document.createElement("span");
+        errorMsg.id = "login-error";
+        errorMsg.className = "message-error";
+    }
+
     let inputEmail = document.querySelector("[name='email']");
     let inputPassword = document.querySelector("[name='password']");
 
-    // On récupère les valeurs dans login
+    // récupère les valeurs dans login
     const login = {
-    email: inputEmail.value,
-    password: inputPassword.value,
+        email: inputEmail.value,
+        password: inputPassword.value,
     };
     // console.log(login);
 
     // vérifie que les champs ne sont pas vide
     if (!login.email || !login.password) {
-        inputPassword.insertAdjacentElement("afterend",errorMsg);
-        errorMsg.className = "message-error-login";
-        errorMsg.innerText = "Veuillez compléter l'ensemble des champs à saisir";
+        errorMsg.innerText = "Veuillez compléter l'ensemble des champs";
     } else {
-
-        // envoi information sur l'API login
         const responseLogin = await fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -31,19 +34,17 @@ form.addEventListener("submit", async function (event) {
         });
 
         const response = await responseLogin.json();
-        //récupération token et statut de la réponse
         const reponseToken = response.token;
         const reponseState = responseLogin.ok;
 
-        // Retour en fonction du statut de la réponse
         if (reponseState) {
             localStorage.setItem("Token", reponseToken);
             window.location.replace("index.html");
         } else {
-            inputPassword.insertAdjacentElement("afterend",errorMsg);
-            errorMsg.className = "message-error-login";
             errorMsg.innerText = "Erreur dans l'identifiant ou le mot de passe";
         };
     };
-});
 
+    // Ajoutez le message d'erreur à l'élément inputPassword
+    inputPassword.insertAdjacentElement("afterend", errorMsg);
+});
